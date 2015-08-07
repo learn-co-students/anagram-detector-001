e6baec912184a486f8bdfc4036bfb512223fb464
ruth.mesfun <ruth.mesfun@flatironschool.com>
2015-08-07 15:08:08 -0400

    All Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = Array.new
+
+    array.each do |mix_word|
+      if mix_word.split('').sort == @word.split('').sort
+        new_array << mix_word
+      end
+    end
+
+    new_array
+  end
+end
