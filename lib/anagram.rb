bbeff1ad633d55f011a50a03f80b68cf9b8163fe
Chris Aporta <christopher.aporta@gmail.com>
2015-10-11 20:44:14 -0400

    complete lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(str)
+    @str = str
+  end
+
+  attr_accessor :str
+
+  def match(arr)
+    match_arr = []
+    char_arr = @str.split("").sort
+    arr.each do |item|
+      if char_arr == item.split("").sort
+        match_arr << item
+      end
+    end
+    match_arr
+  end
+end
