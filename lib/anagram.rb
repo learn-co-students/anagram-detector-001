653b0ccccfd2a5dc647108c872157c9645064848
thaik1618 <kthai1618@gmail.com>
2016-02-28 23:55:50 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    array.each do |poss|
+      poss.chars.to_a.sort == @word.chars.to_a.sort ? matches.push(poss) : nil
+      binding.pry
+    end
+    matches
+  end
+end
