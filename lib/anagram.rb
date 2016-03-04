fb4036b147b9a460ff42ea316746989f6f6623b6
lwadya <lukaswadya@yahoo.com>
2016-03-04 00:14:35 -0500

    Finished lesson

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(match_array)
+    word_ordered = self.order_word(@word)
+    match_array.select { |m|
+      m_ordered = self.order_word(m)
+      word_ordered.eql? m_ordered
+    }
+  end
+
+  def order_word(word)
+    word.split("").sort.join
+  end
+
+end
