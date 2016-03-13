1cb86f91c8e61785d0e495b0aa6200fc83ff1b92
epang1 <epang1@gmail.com>
2016-03-13 16:11:58 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_input = @word.split("").sort
+    counter = 0
+    matched_word = []
+
+    while counter < array.count
+      sorted_word = array[counter].split("").sort
+      if sorted_word == word_input
+        matched_word << array[counter]
+        counter += 1
+      else
+        counter += 1
+      end
+    end
+
+    matched_word
+  end
+
+end
